98579d493f8f0353550880ab4786a8bd942cf725
MrFlacko <35322515+MrFlacko@users.noreply.github.com>
2021-09-11 23:41:11 +1000

    Updating Start.sh
    Made it work a bit nicer, you can run it whenever it starts the server. The script installs the forge server to be used as well

--- Server/start.sh
+++ Server/start.sh
@@ -1,2 +1,16 @@
-#!/usr/bin/env bash
-java -jar server.jar
+#!/bin/sh
+
+## Global Variables
+ramAmount="4G"
+javaArgs="-XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AlwaysPreTouch"
+forgeVersion="forge-1.16.5-36.2.2"
+
+## Install Minecraft Forge if it isn't already installed.
+forge=./${forgeVersion}.jar
+[ ! -f "${forge}" ] && java -jar ./${forgeVersion}-installer.jar --installServer
+
+## Starting the server
+echo "Starting Server..."; java -jar -Xms${ramAmount} -Xmx${ramAmount} ${javaArgs} ${forge} nogui
+
+## When Server Stops
+read -p "Press enter when finished."
